71d975e88ea11cdec952cb57152f02da66730a0b
daihui <spartazhangzhang@gamil.com>
2023-01-09 19:20:41 +0800

    day9-part1

--- day9-generics-trait-lifetimes/src/lib.rs
+++ day9-generics-trait-lifetimes/src/lib.rs
@@ -151,7 +151,21 @@ let float = Point2{x:2.1, y:21.1};
 let p = Point3{x:5, y:"hello"};
 
 //5
+let x = Val{val: 3.0};
+let y = Val{val: "hello".to_string()};
+println!("{}, {}", x.value(), y.value());
 
+
+}
+
+struct Val<T>{
+    val: T,
+}
+
+impl<T> Val<T>{
+    fn value(&self)-> &T{
+        &self.val
+    }
 }
 
 
