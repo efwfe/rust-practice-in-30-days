b9c7ecca5db4f7f78e8d9988efcca0bcc8e9ae90
daihui <spartazhangzhang@gamil.com>
2023-02-13 15:42:01 +0800

    add io opertates

--- io-operate/src/main.rs
+++ io-operate/src/main.rs
@@ -1,20 +1,62 @@
-use std::io;
-use std::io::{Read, Write};
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
+
+use clap::{App, Arg};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main(){
+fn main() -> Result<()> {
+    let matches = App::new("pipeviewer")
+        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
+        .arg(
+            Arg::with_name("outfile")
+                .short("o")
+                .long("outfile")
+                .takes_value(true)
+                .help("Write output to a file instead of stdout"),
+        )
+        .arg(Arg::with_name("silent").short("s").long("silent"))
+        .get_matches();
+
+    let infile = matches.value_of("infile").unwrap_or_default();
+    let outfile = matches.value_of("outfile").unwrap_or_default();
+
+    let silent = if matches.is_present("silent") {
+        true
+    } else {
+        !env::var("PV_SILENT").unwrap_or_default().is_empty()
+    };
+
+    let mut reader: Box<dyn Read> = if !infile.is_empty() {
+        Box::new(BufReader::new(File::open(infile)?))
+    } else {
+        Box::new(io::stdin())
+    };
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(io::stdout())
+    };
+
     let mut total_bytes = 0;
-    loop{
-        let mut buffer = [0; CHUNK_SIZE];
-        let num_read = match io::stdin().read(&mut buffer){
+    let mut buffer = [0; CHUNK_SIZE];
+    loop {
+        let num_read = match reader.read(&mut buffer) {
             Ok(0) => break,
-            Ok(n)=>n,
-             _=>break,
+            Ok(n) => n,
+            _ => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer).unwrap();
+        if let Err(e) = writer.write_all(&buffer) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
     }
-    eprintln!("total: {}", total_bytes);
-
+    if !silent {
+        eprintln!("total: {}", total_bytes);
+    }
+    Ok(())
 }
