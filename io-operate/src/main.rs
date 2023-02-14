6c5088343ce637369197003de383d17502d74429
daihui <spartazhangzhang@gamil.com>
2023-02-14 17:21:47 +0800

    seperate modules

--- io-operate/src/main.rs
+++ io-operate/src/main.rs
@@ -1,62 +1,25 @@
+use pipeviewer::{args::Args, read, stats, write, args};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufReader, BufWriter, ErrorKind, Read, Result, Write};
 
-use clap::{App, Arg};
-
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() -> Result<()> {
-    let matches = App::new("pipeviewer")
-        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
-        .arg(
-            Arg::with_name("outfile")
-                .short("o")
-                .long("outfile")
-                .takes_value(true)
-                .help("Write output to a file instead of stdout"),
-        )
-        .arg(Arg::with_name("silent").short("s").long("silent"))
-        .get_matches();
-
-    let infile = matches.value_of("infile").unwrap_or_default();
-    let outfile = matches.value_of("outfile").unwrap_or_default();
-
-    let silent = if matches.is_present("silent") {
-        true
-    } else {
-        !env::var("PV_SILENT").unwrap_or_default().is_empty()
-    };
-
-    let mut reader: Box<dyn Read> = if !infile.is_empty() {
-        Box::new(BufReader::new(File::open(infile)?))
-    } else {
-        Box::new(io::stdin())
-    };
-    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-        Box::new(BufWriter::new(File::create(outfile)?))
-    } else {
-        Box::new(io::stdout())
-    };
-
+    let args = Args::parse();
     let mut total_bytes = 0;
-    let mut buffer = [0; CHUNK_SIZE];
+
     loop {
-        let num_read = match reader.read(&mut buffer) {
-            Ok(0) => break,
+        let buffer = match read::read(&args.infile) {
+            Ok(x) if x.is_empty() => break,
             Ok(n) => n,
             _ => break,
         };
-        total_bytes += num_read;
-        if let Err(e) = writer.write_all(&buffer) {
-            if e.kind() == ErrorKind::BrokenPipe {
-                break;
-            }
-            return Err(e);
+        stats::stats(args.silent, buffer.len(), &mut total_bytes, false);
+        if !write::write(&args.outfile, &buffer)?{
+            break;
         }
     }
-    if !silent {
-        eprintln!("total: {}", total_bytes);
-    }
+    stats::stats(args.silent, 0, &mut total_bytes, true);
     Ok(())
 }
